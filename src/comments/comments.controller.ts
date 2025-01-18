c7a543c2ac7cb9ffb52d943dbd748711719a3555
Harshil <harshil.karia.hk@gmail.com>
2025-01-18 23:52:01 +0530

    Worked on comment feature

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { CommentsService } from './comments.service';
+import { GetCurrentUser, GetCurrentUserId } from 'src/auth/common/decorator';
+import { CommentDto } from './dto';
 
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+    constructor(
+        private commentsService: CommentsService
+    ) {}
+
+    @Post(':postId')
+    async addComment(
+        @Param('postId',ParseIntPipe) postId: number,
+        @Body() dto: CommentDto,
+        @GetCurrentUserId() userId: number 
+    ) {
+        return this.commentsService.addComment(postId,dto,userId)
+    }
+
+    @Delete('deleteComment/:commentId')
+    async deleteComment(
+        @Param('commentId',ParseIntPipe) commentId: number,
+        @GetCurrentUserId() userId: number
+    ) {
+        return this.commentsService.deleteComment(commentId,userId)
+    }
+
+    @Patch('updateComment/:commentId')
+    async updateComment(
+        @Param('commentId',ParseIntPipe) commentId: number,
+        @Body() dto: CommentDto,
+        @GetCurrentUserId() userId: number
+    ) {
+        return this.commentsService.updateComment(commentId,dto,userId)
+    }
+
+
+}
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,102 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthService } from 'src/auth/auth.service';
+import { CommentDto } from './dto';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+    constructor(
+        private prisma: PrismaService,
+        private authService: AuthService
+    ) {}
+
+    async addComment(postId: number, dto: CommentDto, userId: number) {
+        const post = await this.prisma.post.findUnique({
+            where: {
+                id: postId
+            }
+        })
+
+        if(!post) {
+            throw new NotFoundException('Invalid Post Id')
+        }
+
+        const updatedPost = await this.prisma.post.update({
+            where: {
+                id: postId
+            },
+            data: {
+                comments: {
+                    create: {
+                        comment: dto.comment,
+                        userId
+                    }
+                }
+            }
+        })
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+        const tokens = await this.authService.getTokens(userId,user.email)
+        const at = tokens.access_token
+        return {at,updatedPost}
+    }
+
+    async deleteComment(commentId: number,userId: number) {
+        const comment = await this.prisma.comment.findUnique({
+            where: {
+                id: commentId
+            }
+        })
+
+        if(!comment) {
+            throw new NotFoundException('Invalid Comment Id')
+        }
+        if(comment.userId !== userId) {
+            throw new ForbiddenException('Invalid User to delete comment')
+        }
+        const deleteComment = await this.prisma.comment.delete({
+            where: {
+                id: commentId
+            }
+        })
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+        const tokens = await this.authService.getTokens(userId,user.email)
+        const at = tokens.access_token
+        return {at,deleteComment}
+    }
+
+    async updateComment(commentId: number,dto: CommentDto,userId: number) {
+        const comment = await this.prisma.comment.findUnique({
+            where: {
+                id: commentId
+            }
+        })
+        if(!comment) {
+            throw new NotFoundException('Comment not found')
+        }
+
+        const updateComment = await this.prisma.comment.update({
+            where: {
+                id: commentId
+            },
+            data: {
+                comment: dto.comment
+            }
+        })
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+        const tokens = await this.authService.getTokens(userId,user.email)
+        const at = tokens.access_token
+        return {at,updateComment}
+    }
+}
